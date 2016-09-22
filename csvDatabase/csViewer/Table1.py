ac6c52bc1994b38175e44813f44866735f34dd12
ShriramSunder <ss6g11@soton.ac.uk>
2016-09-22 18:49:46 +0400

    Just before leaving 22-Sep-16, preserving state

--- csvDatabase/csViewer/Table1.py
+++ csvDatabase/csViewer/Table1.py
@@ -1,67 +1,83 @@
 import csv
 
+root = "C:/Python/Django_Files/Data/"
+filename = "full_data.csv"
+filename2 = "cost_forecasting.csv"
+name = 'cleaned_data.csv'
+
+file = root + filename
+file2 = root + filename2
+new_filename = root + name
+
 class Tab1():
 	## make unique list from list
 	def make_unique(lst):
-		Ulst = []
+		Ulist = []
 		for i in range(len(lst) - 1):
 			for j in range(len(lst) - 1):
 				if lst[i] == lst[j]:
-					if lst[i] in Ulst:
+					if lst[i] in Ulist:
 						pass
 					else:
-						Ulist.appen(lst[i])
-		return(Ulst)
+						Ulist.append(lst[i])
+		return(Ulist)
 
-	## for field 'Prj_nos'
+	## for field 'P_nos'
 	def find_no_of_projects_for_each_pm(filename):
-		prj_names = []
-		prj_manager = []
-		unique_prjnames = []
-		unique_prjmanager = []
+		P_names = []
+		P_manager = []
+		unique_pnames = []
+		unique_pm = []
 		rel = {}
-		prj_nos = {}
+		P_nos = {}
 
 		with open(filename) as csvfile:
 			csvreader = csv.reader(csvfile)
 			for line in csvreader:
-				prj_manager.append(line[2])
-				prj_names.append(line[1])
+				P_manager.append(line[2])
+				P_names.append(line[1])
 				# the project name is related to project manager
 				rel[line[1]] = line[2]
 
-		for i in range(len(prj_names) - 1):
-			for j in range(len(prj_names) - 1): 
-				if prj_manager[i] == prj_manager[j]:
-					if prj_manager[i] in unique_prjmanager:
-						pass
-					else: 
-						unique_prjmanager.append(prj_manager[i])
+		# after file closes, make unique list
+		unique_pm = make_unique(P_names)
 
-		for i in unique_prjmanager: 
+		# find the number of unique pm's in the original list
+		for i in unique_pm: 
 			s = 0
-			for j in prj_names:
+			for j in P_names:
 				if rel[j] == i:
 					s = s + 1
-			prj_nos[i] = s
+			P_nos[i] = s
 
-		return(prj_nos)
+		return(P_nos)
 
 	## for field 'PS1'
 	def find_sum_of_PS1_for_each_pm(filename):
-		PS1 = [];s = [];pm = [];unique_pm = []
+		PS1 = [] 
+		pm = []
+		unique_pm = []
 		# use rel for storing the final values
 		# rel contains pm names, and their ps1's
 		rel = {}
+		individual_s = 0
+		s = {}
 
 		with open(filename) as csvfile:
 			csvreader = csv.reader(csvfile)
 			for line in csvreader:
 				# store all these names
-				# and make them unique later
-				pm.append(line[2])
-				PS1.append(line[7])
+				# and make them unique later	
+				
+				pm.append([line[2]])
 
+			# make unique pm list 
+			unique_pm = Tab1.make_unique(pm)
+			
+			csvfile.seek(0)
+			for line in csvreader:
+				for i in unique_pm:
+					if i in 
 	## for field 'PS4'
 	## for field 'PS1vsPS4'
 	## for field 'Q1 sales'
@@ -70,3 +86,4 @@ class Tab1():
 	## for field 'Q4 sales'
 	## for field 'cr'
 	
+Tab1.find_sum_of_PS1_for_each_pm(new_filename)
