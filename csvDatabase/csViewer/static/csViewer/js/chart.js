59009fb4d38fb3a79863b59d09cfe2592e573681
ShriramSunder <ss6g11@soton.ac.uk>
2016-10-24 12:47:25 +0400

    Made graph x-ticks rotate successfully, by adding D3fc library.

--- csvDatabase/csViewer/static/csViewer/js/chart.js
+++ csvDatabase/csViewer/static/csViewer/js/chart.js
@@ -2,32 +2,48 @@ function makeGraphs(data) {
 	
 	//create a crossfilter instance
 	//named after n-dimentions
-	console.log(data)
+
+	var width = 1000, height = 500;
+	
 	var ndx = crossfilter(data);
-	// console.log(ndx)
-	var dim = ndx.dimension(function(d){ return(d['PS1']);});
+	var dim = ndx.dimension(function(d){ return d.PS1;});
 	var grp = dim.group();
 
-	dc.barChart("#PM-Chart")
-		.dimension(dim)
-		.width(1000)
-		// .height(200)
-		.group(grp)
-		.x(d3.scale.ordinal().domain(data.map(function (d) {
+	var ordinalScale = d3.scale.ordinal()
+		.domain(data.map(function (d) {
 			// body...
-			return d.PM.slice(0,2);
-		})))
-		.y(d3.scale.linear().domain([0,5]))
-		// .y(d3.scale.linear().domain(data.map(function(d){
-		// 	// body...
-		// 	console.log(Math.round(d.PS1)/100000);
-		// 	return Math.round(d.PS1)/100000;
-		// })))
-		.xUnits(dc.units.ordinal)
-		.brushOn(true)
-		.centerBar(true)
-		.xAxisPadding(500)
-		.yAxisPadding(10);
+			return d.PM;
+		}))
+		.rangePoints([0,width],1);
+
+	// var yAxis2 = d3.scale.linear()
+	// 					.domain(data.map(function(d){
+	// 						// console.log(d.PS1);
+	// 						// console.log(Math.round(d.PS1)/1000000);
+	// 						return Math.round(d.PS1)/1000000;
+	// 					}));
+
+	var yAxis = d3.scale.linear()
+						.domain([0,3]);
+
+	var fir = dc.barChart("#PM")
+				.width(width)
+				.height(height)
+				.dimension(dim)
+				.group(grp)
+				.x(ordinalScale)
+				.y(yAxis)
+				.gap(1)
+				.xUnits(dc.units.ordinal)
+				.brushOn(true)
+				.centerBar(true)
+				.renderHorizontalGridLines(true)
+	
+	fir.renderlet(function(chart){
+		chart.selectAll("g.x text")
+		.attr('transform', 'translate(-10,-100) rotate(270)');
+	});
 
 	dc.renderAll();
+
 };
